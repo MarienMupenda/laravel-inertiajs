5a6a114a9a3c826757e498be49cb9b39a23ae2af
Marien Mupenda <marien.mupenda@gmail.com>
2022-05-08 22:15:32 +0200

    clean project

--- resources/views/_layouts/_footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-gray-200">
-    <div class="container flex items-center justify-between px-6 py-3 mx-auto">
-        <a href="#" class="text-xl font-bold text-gray-500 hover:text-gray-400">{{ config('app.name') }}</a>
-        <p class="py-2 text-gray-500 sm:py-0">Produit de SmirlTech</p>
-    </div>
-</footer>
